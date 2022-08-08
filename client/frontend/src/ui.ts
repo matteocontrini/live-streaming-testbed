668e0d7070f9221e5d5114fa49fcaefc5e5f0428
Matteo Contrini <matteo@contrini.it>
2022-08-08 10:36:37 +0200

    Fix style

--- client/frontend/src/ui.ts
+++ client/frontend/src/ui.ts
@@ -1,13 +1,14 @@
-import { MediaPlayer, MediaPlayerClass } from "dashjs";
+import {MediaPlayer, MediaPlayerClass} from "dashjs";
 
-export function createPlayer() : MediaPlayerClass {
+export function createPlayer(): MediaPlayerClass {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <video id="player" width="600" height="300" controls></video>
+        <br>
         <button id="play">Play</button>
     `;
 
-    const url = 'https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd';
-    // const url = 'https://cdn.local/manifest.mpd';
+    // const url = 'https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd';
+    const url = 'https://cdn.local/manifest.mpd';
 
     const player = MediaPlayer().create();
     player.initialize(document.querySelector<HTMLVideoElement>('#player')!, url);
@@ -23,4 +24,4 @@ export function createPlayer() : MediaPlayerClass {
     });
 
     return player;
-};
+}
